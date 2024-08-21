a9cc857ca0f0133b3c35fab0dc02dae35e5466fd
Amritanshu312 <ffdarkrayon@gmail.com>
2024-08-21 21:57:41 +0530

    [collections] feat🎉: Header

--- app/(pages)/collections/page.js
+++ app/(pages)/collections/page.js
@@ -1,7 +1,23 @@
+import { BsThreeDots } from "react-icons/bs"
+
 const Collections = () => {
   return (
-    <div>
-      Collections
+    <div className="w-full min-h-screen flex">
+
+      <div className="w-full mt-28 max-[635px]:mt-12 flex items-center flex-col max-[745px] mx-4">
+
+        <div className="w-full max-[1226px]:ml-[16rem] max-[1090px]:ml-1 max-w-[800px] max-[1226px]:max-w-[712px]">
+          <div className="flex justify-between items-center mb-16">
+            <div className="font-['poppins'] text-2xl font-medium">Collections</div>
+
+            <div className="cursor-pointer text-xl text-[#bdbdc0]"><BsThreeDots /></div>
+          </div>
+
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
--- utils/useWindowDimension.jsx
+++ utils/useWindowDimension.jsx
@@ -3,12 +3,22 @@
 import { useEffect, useState } from "react";
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    w: width,
-    h: height
-  };
+  // Check if window is defined to avoid errors during SSR
+  if (typeof window !== "undefined") {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      w: width,
+      h: height
+    };
+  } else {
+    // Provide a default value for server-side rendering
+    return {
+      w: 0,
+      h: 0
+    };
+  }
 }
+
 
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
