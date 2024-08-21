6d34778c3ebd0324b5c1879d4140ca68e79edb0f
Amritanshu312 <ffdarkrayon@gmail.com>
2024-08-21 21:21:29 +0530

    [WelcomeInfo] fix 🩹: Readable Time

--- content/Home/WelcomeInfo.jsx
+++ content/Home/WelcomeInfo.jsx
@@ -9,7 +9,7 @@ const WelcomeInfo = () => {
 
   return (
     <div className="text-4xl tracking-normal text-[#dcdcdf] font-['poppins'] font-medium flex flex-col gap-2">
-      <div>Good morning,</div>
+      <div>Good {new Date().getHours() < 12 ? 'morning' : new Date().getHours() < 18 ? 'afternoon' : 'evening'},</div>
       <div className="ml-1 text-[#efeff4]">
         {loading ? "Guest" : name.charAt(0).toUpperCase() + name.slice(1)}
       </div>
